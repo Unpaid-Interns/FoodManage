7f207ec09792d656bb3284ab373118bf7758ebd5
Marcus <mlo11@duke.edu>
2019-01-23 14:49:18 -0500

    in class work on checking database

--- food_site/importer/CSVImport.py
+++ food_site/importer/CSVImport.py
@@ -1,6 +1,7 @@
 import sys
 import csv
 from importer import CSVData
+
 # from sku_manage import models
 
 # test = "skus"
@@ -21,18 +22,26 @@ class CSVImport():
         self.number_records_dict = dict()
 
     def parse(self):
+        import_completed_successfully = True
         for filename in self.filenames:
             file_prefix, valid = prefix_check(filename)
             if (valid):
-                data, num_records, success = parser(filename)
+                data, num_records, success, error_message = parser(filename)
                 if (success):
                     self.data_dict[file_prefix] = data
                     self.number_records_dict[file_prefix] = num_records
                 else:
-                    print("Parsing failed for: " + filename)
+                    print("Import failed for: " + filename)
+                    print(error_message)
+                    import_completed_successfully = False
             else:
-                print("Invalid file prefix for: " + filename)
-        # self.commit_to_database()
+                print("Import failed for: " + filename)
+                print("ERROR: file name " + filename + " is invalid, must begin with valid prefix 'skus', \
+                        'ingredients', 'product_lines', or 'sku_ingredients'")
+                import_completed_successfully = False
+        if import_completed_successfully:
+            pass
+            # self.commit_to_database()
 
     def add_filename(self, filename):
         self.filenames.append(filename)
@@ -64,16 +73,21 @@ class CSVImport():
     #         if(p == "sku_ingredients"):
     #             models.IngredientQty.objects.bulk_create(models_array)
 
+
 def parser(filename):
+    # Check for valid prefix in file name
     file_prefix, valid = prefix_check(filename)
     if not valid:
         print("FILE NAME INVALID: Terminating import...")
-        return None, None, False
+        return None, None, False, "ERROR: file name " + filename + " is invalid, must begin with valid prefix 'skus', \
+        'ingredients', 'product_lines', or 'sku_ingredients'"
 
+    # Initialize variables
     header_correct = headerDict[file_prefix + ".csv"]
     parsed_data = []
     num_records_imported = 0
 
+    # Open the csv file, read only
     with open(filename, 'r') as csvfile:
         data_reader = csv.reader(csvfile, quotechar='|')
         header_valid = True
@@ -87,22 +101,25 @@ def parser(filename):
             if not header_valid:
                 print("HEADER INVALID: Terminating import...")
                 print(header_issue)
-                return
+                return None, None, False, header_issue
             method_to_call = getattr(sys.modules[__name__], file_prefix + "_parser_helper")
-            temp_data = method_to_call(row)
-            if (check_for_identical_record(temp_data)):
-                if (check_for_match_name_or_id):
-                    parsed_data.append(temp_data)
+            temp_data = method_to_call(row, num_records_imported)
+            if isinstance(temp_data, str):
+                return None, None, False, temp_data
+            if check_for_identical_record(temp_data, file_prefix):
+                if check_for_match_name_or_id:
+                    parsed_data.append(temp_data, file_prefix)
                     num_records_imported += 1
+                else:
+                    return None, None, False, "ERROR: Duplicate name/ID in csv file"
 
-    return parsed_data, num_records_imported, True
+    return parsed_data, num_records_imported, True, ""
 
 
-def skus_parser_helper(row):
+def skus_parser_helper(row, num_records_imported):
     if len(row) != 8:
-        print("Problem with number of entries in row:")
-        print(row)
-        return
+        return ("ERROR: Problem with number of entries in row #" + (num_records_imported + 1) +
+                ", needs 8 entries and has " + row.__len__)
     return CSVData.SKUData(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
 
 
@@ -120,6 +137,37 @@ def sku_ingredients_parser_helper(row):
 
 def check_for_identical_record(record):
     # TODO: check database to see if COMPLETELY IDENTICAL record already exists here
+    # Returns True if no identical record found
+    # Could use filter to check for same name and ID but not other fields
+    record_converted = record.convert_to_database_model()
+    if (p == "skus"):
+        list = models.SKU.objects.filter(name=record.name)
+        for item in list:
+            if (item.name == record_converted.name and item.sku_num == record_converted.sku_num and
+                    item.case_upc == record_converted.case_upc and item.unit_upc == record_converted.unit_upc
+                    and item.unit_size == record_converted.unit_size and item.units_per_case ==
+                    record_converted.units_per_case and item.product_line == record_converted.product_line and
+                    item.comment == record_converted.comment):
+                return False
+    if (p == "ingredients"):
+        list = models.Ingredient.objects.filter(name=record.name)
+        for item in list:
+            if (item.name == record_converted.name and item.number == record_converted.number
+                    and item.vendor_info == record_converted.vendor_info and
+                    item.package_size == record_converted.package_size and item.cost == record_converted.cost
+                    and item.comment == record_converted.comment):
+                return False
+    if (p == "product_lines"):
+        list = models.ProductLine.objects.filter(name=record.name)
+        for item in list:
+            if (item.name == record_converted.name):
+                return False
+    if (p == "sku_ingredients"):
+        list = models.IngredientQty.objects.filter(sku=record.sku_number)
+        for item in list:
+            if (item.sku == record_converted.sku and item.ingredient == record_converted.ingredient
+                    and item.quantity == record_converted.quantity):
+                return False
     return True
 
 
@@ -132,7 +180,7 @@ def header_check(header, headerCorrect):
     col = 0
     for item in header:
         if headerCorrect[col] != item:
-            headerError = "ERROR: header = '" + item + "' but should be '" + headerCorrect[col] + "'"
+            headerError = "ERROR: csv header = '" + item + "' but should be '" + headerCorrect[col] + "'"
             return False, headerError
         col += 1
     return True, ""
