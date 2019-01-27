983ae849c683c252a59789619176dad3e5ec82a9
Nathaniel Brooke <neb17@duke.edu>
2019-01-27 12:05:08 -0500

    Additional admin site customizations

--- food_site/sku_manage/admin.py
+++ food_site/sku_manage/admin.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import Group, User
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
 from django.contrib import admin
 
 from .models import Ingredient, ProductLine, SKU, IngredientQty
+
+class MainAdmin(admin.AdminSite):
+	site_header = 'Hypothetical Meals'
+	site_title = 'Hypothetical Admin'
+	index_title = 'Hypothetical Meals Administration'
+
 
 class IngredientAdmin(admin.ModelAdmin):
 	fields = ['name', 'number', 'package_size', 'cost', 'vendor_info', 'comment']
@@ -23,6 +31,9 @@ class SkuAdmin(admin.ModelAdmin):
 	list_filter = ['product_line']
 	search_fields = ['name', 'sku_num']
 
-admin.site.register(Ingredient, IngredientAdmin)
-admin.site.register(SKU, SkuAdmin)
-admin.site.register(ProductLine)
+admin_site = MainAdmin(name='food_site_admin')
+admin_site.register(Ingredient, IngredientAdmin)
+admin_site.register(SKU, SkuAdmin)
+admin_site.register(ProductLine)
+admin_site.register(Group, GroupAdmin)
+admin_site.register(User, UserAdmin)
--- food_site/sku_manage/apps.py
+++ food_site/sku_manage/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class SkuManageConfig(AppConfig):
     name = 'sku_manage'
+    verbose_name = 'SKU Data Management'
