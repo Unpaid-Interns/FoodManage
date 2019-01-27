1541047848dc1c35023602eec0f5ed20c81a4d32
Nathaniel Brooke <neb17@duke.edu>
2019-01-27 17:02:23 -0500

    Updated data structure and admin page to more carefully type check

--- food_site/sku_manage/models.py
+++ food_site/sku_manage/models.py
@@ -1,15 +1,27 @@
 from django.db import models
+import random
 
 class Ingredient(models.Model):
 	name = models.CharField(max_length=256, unique=True, verbose_name='Ingredient Name')
-	number = models.IntegerField(primary_key=True, verbose_name='Ingredient#')
+	number = models.IntegerField(unique=True, blank=True, verbose_name='Ingredient#')
 	vendor_info = models.TextField(blank=True, verbose_name='Vendor Information')
 	package_size = models.CharField(max_length=256)
 	cost = models.DecimalField(max_digits=12, decimal_places=2)
 	comment = models.TextField(blank=True)
 
+	def gen_num(self):
+		ingredients = Ingredient.objects.order_by('number')
+		for i in range(0, len(ingredients) + 1):
+			if(ingredients[i].number > i):
+				return i
+
 	def __str__(self):
 		return self.name
+
+	def save(self):
+		if not self.number:
+			self.number = self.gen_num()
+		super(Ingredient, self).save()
 
 class ProductLine(models.Model):
 	name = models.CharField(max_length=256, primary_key=True)
@@ -19,7 +31,7 @@ class ProductLine(models.Model):
 
 class SKU(models.Model):
 	name = models.CharField(max_length=32)
-	sku_num = models.IntegerField(primary_key=True, verbose_name='SKU#')
+	sku_num = models.IntegerField(unique=True, blank=True, verbose_name='SKU#')
 	case_upc = models.DecimalField(max_digits=12, decimal_places=0, unique=True, verbose_name='Case UPC')
 	unit_upc = models.DecimalField(max_digits=12, decimal_places=0, verbose_name='Unit UPC')
 	unit_size = models.CharField(max_length=256)
@@ -27,8 +39,35 @@ class SKU(models.Model):
 	product_line = models.ForeignKey(ProductLine, on_delete=models.PROTECT)
 	comment = models.TextField(blank=True)
 
+	def check_case_upc(self):
+		upc_str = '0' * (12-len(self.case_upc)) + str(self.case_upc)
+		if 0 < int(upc_str[0]) < 6:
+			return False
+		sum = 0
+		for i in range(0, 5):
+			sum += int(upc_str[i*2])
+		sum = sum * 3
+		for i in range(0, 5):
+			sum += int(upc_str[i*2 + 1])
+		return sum % 10 == 0
+
+	def check_unit_upc(self):
+
+
+	def gen_num(self):
+	skus = SKU.objects.order_by('number')
+	for i in range(0, len(skus) + 1):
+		if(skus[i].sku_num > i):
+			return i
+
 	def __str__(self):
 		return "{name}: {unit_size} * {units_per_case}".format(name=self.name, unit_size=self.unit_size, units_per_case=self.units_per_case) 
+
+	def save(self):
+		if not self.sku_num:
+			self.sku_num = self.gen_num()
+		super(SKU, self).save()
+
 
 class IngredientQty(models.Model):
 	sku = models.ForeignKey(SKU, on_delete=models.CASCADE)
