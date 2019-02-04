514e99bab4c95c4b06ede0989d635a21bc82d43b
Nathaniel Brooke <neb17@duke.edu>
2019-02-03 22:05:04 -0500

    Fixed model test

--- food_site/sku_manage/tests.py
+++ food_site/sku_manage/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, TransactionTestCase
 from django.db.utils import IntegrityError
+from django.core.exceptions import ValidationError
 from . import models
 
 
@@ -139,146 +140,70 @@ class SKUUniqueTests(TransactionTestCase):
 			sku2.save()
 
 
-class SkuCaseUpcTests(TestCase):
+class UPCValidationTests(TestCase):
 
 	def test_all_zeros(self):
-		sku = models.SKU(case_upc = int('000000000000'))
-		self.assertIs(sku.check_case_upc(), True)
+		models.validate_upc(int('000000000000'))
 
 	def test_correct_check_1(self):
-		sku = models.SKU(case_upc = int('000400000006'))
-		self.assertIs(sku.check_case_upc(), True)
+		models.validate_upc(int('000400000006'))
 
 	def test_correct_check_2(self):
-		sku = models.SKU(case_upc = int('004000000008'))
-		self.assertIs(sku.check_case_upc(), True)
+		models.validate_upc(int('004000000008'))
 
 	def test_correct_check_3(self):
-		sku = models.SKU(case_upc = int('041104256290'))
-		self.assertIs(sku.check_case_upc(), True)
+		models.validate_upc(int('041104256290'))
 
 	def test_correct_check_4(self):
-		sku = models.SKU(case_upc = int('019556793038'))
-		self.assertIs(sku.check_case_upc(), True)
+		models.validate_upc(int('019556793038'))
 
 	def test_correct_check_5(self):
-		sku = models.SKU(case_upc = int('766171000439'))
-		self.assertIs(sku.check_case_upc(), True)
+		models.validate_upc(int('766171000439'))
 
 	def test_nonzero_first_digit_6(self):
-		sku = models.SKU(case_upc = int('600000000002'))
-		self.assertIs(sku.check_case_upc(), True)
+		models.validate_upc(int('600000000002'))
 
 	def test_nonzero_first_digit_7(self):
-		sku = models.SKU(case_upc = int('700000000009'))
-		self.assertIs(sku.check_case_upc(), True)
+		models.validate_upc(int('700000000009'))
 
 	def test_nonzero_first_digit_8(self):
-		sku = models.SKU(case_upc = int('800000000006'))
-		self.assertIs(sku.check_case_upc(), True)
+		models.validate_upc(int('800000000006'))
 
 	def test_nonzero_first_digit_9(self):
-		sku = models.SKU(case_upc = int('900000000003'))
-		self.assertIs(sku.check_case_upc(), True)
+		models.validate_upc(int('900000000003'))
 
 	def test_bad_check_1(self):
-		sku = models.SKU(case_upc = int('000000000001'))
-		self.assertIs(sku.check_case_upc(), False)
+		with self.assertRaisesMessage(ValidationError, 'UPC check digit is not valid'):
+			models.validate_upc(int('000000000001'))
 
 	def test_bad_check_2(self):
-		sku = models.SKU(case_upc = int('000000010000'))
-		self.assertIs(sku.check_case_upc(), False)
+		with self.assertRaisesMessage(ValidationError, 'UPC check digit is not valid'):
+			models.validate_upc(int('000000010000'))
 
 	def test_bad_first_digit_1(self):
-		sku = models.SKU(case_upc = int('100000000000'))
-		self.assertIs(sku.check_case_upc(), False)
+		with self.assertRaisesMessage(ValidationError, 'UPC first digit is not valid'):
+			models.validate_upc(int('100000000000'))
 
 	def test_bad_first_digit_2(self):
-		sku = models.SKU(case_upc = int('200000000000'))
-		self.assertIs(sku.check_case_upc(), False)
+		with self.assertRaisesMessage(ValidationError, 'UPC first digit is not valid'):
+			models.validate_upc(int('200000000000'))
 
 	def test_bad_first_digit_3(self):
-		sku = models.SKU(case_upc = int('300000000000'))
-		self.assertIs(sku.check_case_upc(), False)
+		with self.assertRaisesMessage(ValidationError, 'UPC first digit is not valid'):
+			models.validate_upc(int('300000000000'))
 
 	def test_bad_first_digit_4(self):
-		sku = models.SKU(case_upc = int('400000000000'))
-		self.assertIs(sku.check_case_upc(), False)
+		with self.assertRaisesMessage(ValidationError, 'UPC first digit is not valid'):
+			models.validate_upc(int('400000000000'))
 
 	def test_bad_first_digit_5(self):
-		sku = models.SKU(case_upc = int('500000000000'))
-		self.assertIs(sku.check_case_upc(), False)
+		with self.assertRaisesMessage(ValidationError, 'UPC first digit is not valid'):
+			models.validate_upc(int('500000000000'))
 
+	def test_bad_negative(self):
+		with self.assertRaisesMessage(ValidationError, 'UPC cannot be negative'):
+			models.validate_upc(int('-000000000019'))
 
-class SkuUnitUpcTests(TestCase):
-
-	def test_all_zeros(self):
-		sku = models.SKU(unit_upc = int('000000000000'))
-		self.assertIs(sku.check_unit_upc(), True)
-
-	def test_correct_check_1(self):
-		sku = models.SKU(unit_upc = int('000400000006'))
-		self.assertIs(sku.check_unit_upc(), True)
-
-	def test_correct_check_2(self):
-		sku = models.SKU(unit_upc = int('004000000008'))
-		self.assertIs(sku.check_unit_upc(), True)
-
-	def test_correct_check_3(self):
-		sku = models.SKU(unit_upc = int('041104256290'))
-		self.assertIs(sku.check_unit_upc(), True)
-
-	def test_correct_check_4(self):
-		sku = models.SKU(unit_upc = int('019556793038'))
-		self.assertIs(sku.check_unit_upc(), True)
-
-	def test_correct_check_5(self):
-		sku = models.SKU(unit_upc = int('766171000439'))
-		self.assertIs(sku.check_unit_upc(), True)
-
-	def test_nonzero_first_digit_6(self):
-		sku = models.SKU(unit_upc = int('600000000002'))
-		self.assertIs(sku.check_unit_upc(), True)
-
-	def test_nonzero_first_digit_7(self):
-		sku = models.SKU(unit_upc = int('700000000009'))
-		self.assertIs(sku.check_unit_upc(), True)
-
-	def test_nonzero_first_digit_8(self):
-		sku = models.SKU(unit_upc = int('800000000006'))
-		self.assertIs(sku.check_unit_upc(), True)
-
-	def test_nonzero_first_digit_9(self):
-		sku = models.SKU(unit_upc = int('900000000003'))
-		self.assertIs(sku.check_unit_upc(), True)
-
-	def test_bad_check_1(self):
-		sku = models.SKU(unit_upc = int('000000000001'))
-		self.assertIs(sku.check_unit_upc(), False)
-
-	def test_bad_check_2(self):
-		sku = models.SKU(unit_upc = int('000000010000'))
-		self.assertIs(sku.check_unit_upc(), False)
-
-	def test_bad_first_digit_1(self):
-		sku = models.SKU(unit_upc = int('100000000000'))
-		self.assertIs(sku.check_unit_upc(), False)
-
-	def test_bad_first_digit_2(self):
-		sku = models.SKU(unit_upc = int('200000000000'))
-		self.assertIs(sku.check_unit_upc(), False)
-
-	def test_bad_first_digit_3(self):
-		sku = models.SKU(unit_upc = int('300000000000'))
-		self.assertIs(sku.check_unit_upc(), False)
-
-	def test_bad_first_digit_4(self):
-		sku = models.SKU(unit_upc = int('400000000000'))
-		self.assertIs(sku.check_unit_upc(), False)
-
-	def test_bad_first_digit_5(self):
-		sku = models.SKU(unit_upc = int('500000000000'))
-		self.assertIs(sku.check_unit_upc(), False)
 
 
 class DataSaveTests(TestCase):
@@ -339,29 +264,29 @@ class DataSaveTests(TestCase):
 		to_soup = models.SKU(name='Tomato Soup', sku_num=0, case_upc=0, product_line=soups, unit_upc=13, unit_size='Small Box', units_per_case=16)
 		po_soup = models.SKU(name='Potato and Onion Soup', sku_num=1, case_upc=101, product_line=soups, unit_upc=13, unit_size='Small Box', units_per_case=16)
 		cn_soup = models.SKU(name='Chicken Noodle Soup', sku_num=2, case_upc=10001, product_line=soups, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		tu_soup = models.SKU(name='Turkey Soup', sku_num=3, case_upc=1000001, product_line=soups, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		fi_soup = models.SKU(name='Fish Soup', sku_num=4, case_upc=100000001, product_line=soups, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		tu_soup = models.SKU(name='Turkey Soup', sku_num=3, case_upc=1000009, product_line=soups, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		fi_soup = models.SKU(name='Fish Soup', sku_num=4, case_upc=100000009, product_line=soups, unit_upc=13, unit_size='Small Box', units_per_case=16)
 		ptc_soup = models.SKU(name='Potato, Tomato, and Carrot Soup', sku_num=5, case_upc=10000000001, product_line=soups, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		p_salad = models.SKU(name='Potato Salad', sku_num=6, case_upc=202, product_line=salads, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		l_salad = models.SKU(name='Salad', sku_num=7, case_upc=20002, product_line=salads, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		e_salad = models.SKU(name='Egg Salad', sku_num=8, case_upc=2000002, product_line=salads, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		spaghetti = models.SKU(name='Spaghetti', sku_num=9, case_upc=303, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		ctikka = models.SKU(name='Chicken Tikka', sku_num=10, case_upc=30003, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		p_salad = models.SKU(name='Potato Salad', sku_num=6, case_upc=208, product_line=salads, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		l_salad = models.SKU(name='Salad', sku_num=7, case_upc=20008, product_line=salads, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		e_salad = models.SKU(name='Egg Salad', sku_num=8, case_upc=2000008, product_line=salads, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		spaghetti = models.SKU(name='Spaghetti', sku_num=9, case_upc=307, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		ctikka = models.SKU(name='Chicken Tikka', sku_num=10, case_upc=30007, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
 		pizza = models.SKU(name='Pizza', sku_num=11, case_upc=3000003, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		fishnchips = models.SKU(name='Fish-n-Chips', sku_num=12, case_upc=300000003, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		lasagna = models.SKU(name='Lasagna', sku_num=13, case_upc=10102, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		burrito = models.SKU(name='Burrito', sku_num=14, case_upc=1000102, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		ezmac = models.SKU(name='Easy Mac', sku_num=15, case_upc=1010103, product_line=macncheese, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		threemac = models.SKU(name='3 Cheese Mac-n-Cheese', sku_num=16, case_upc=1010204, product_line=macncheese, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		american = models.SKU(name='American Cheese Whip', sku_num=17, case_upc=1020104, product_line=easy_cheeses, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		easycf = models.SKU(name='Cheese Food', sku_num=18, case_upc=2010104, product_line=easy_cheeses, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		ketchup = models.SKU(name='Ketchup', sku_num=19, case_upc=2030106, product_line=condiments, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		fishnchips = models.SKU(name='Fish-n-Chips', sku_num=12, case_upc=300000007, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		lasagna = models.SKU(name='Lasagna', sku_num=13, case_upc=10108, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		burrito = models.SKU(name='Burrito', sku_num=14, case_upc=1000108, product_line=entrees, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		ezmac = models.SKU(name='Easy Mac', sku_num=15, case_upc=1010107, product_line=macncheese, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		threemac = models.SKU(name='3 Cheese Mac-n-Cheese', sku_num=16, case_upc=1010206, product_line=macncheese, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		american = models.SKU(name='American Cheese Whip', sku_num=17, case_upc=1020106, product_line=easy_cheeses, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		easycf = models.SKU(name='Cheese Food', sku_num=18, case_upc=2010106, product_line=easy_cheeses, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		ketchup = models.SKU(name='Ketchup', sku_num=19, case_upc=2030104, product_line=condiments, unit_upc=13, unit_size='Small Box', units_per_case=16)
 		mayo = models.SKU(name='Mayonnaise', sku_num=20, case_upc=2020105, product_line=condiments, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		relish = models.SKU(name='Relish', sku_num=21, case_upc=2030207, product_line=condiments, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		hburger = models.SKU(name='Frozen Hamburger', sku_num=22, case_upc=2040208, product_line=frozen_meat, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		hdog = models.SKU(name='Frozen Hot Dog', sku_num=23, case_upc=3040209, product_line=frozen_meat, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		steak = models.SKU(name='Frozen Steak', sku_num=24, case_upc=3030208, product_line=frozen_meat, unit_upc=13, unit_size='Small Box', units_per_case=16)
-		fturkey = models.SKU(name='Frozen Turkey', sku_num=25, case_upc=3010206, product_line=frozen_meat, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		relish = models.SKU(name='Relish', sku_num=21, case_upc=2030203, product_line=condiments, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		hburger = models.SKU(name='Frozen Hamburger', sku_num=22, case_upc=2040202, product_line=frozen_meat, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		hdog = models.SKU(name='Frozen Hot Dog', sku_num=23, case_upc=3040201, product_line=frozen_meat, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		steak = models.SKU(name='Frozen Steak', sku_num=24, case_upc=3030202, product_line=frozen_meat, unit_upc=13, unit_size='Small Box', units_per_case=16)
+		fturkey = models.SKU(name='Frozen Turkey', sku_num=25, case_upc=3010204, product_line=frozen_meat, unit_upc=13, unit_size='Small Box', units_per_case=16)
 		to_soup.save()
 		po_soup.save()
 		cn_soup.save()
