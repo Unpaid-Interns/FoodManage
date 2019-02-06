c0f5819cd997b70bab37041252b83d193d2599ce
Nathaniel Brooke <neb17@duke.edu>
2019-02-05 22:47:53 -0500

    Install now configures Django settings

--- food_site/food_site/settings.py
+++ food_site/food_site/settings.py
@@ -93,14 +93,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'db_name',
-    #     'USER': 'db_user',
-    #     'PASSWORD': 'db_password',
-    #     'HOST': '',
-    #     'PORT': '',
-    # }
 }
 
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -24,10 +24,12 @@ sudo apt-get -qq -y install certbot python-certbot-apache
 sudo certbot --apache
 
 # Install and configure postgres
+echo "Enter a password to use with your database"
+read DB_PASS
 sudo apt-get -qq -y install postgresql postgresql-contrib
 if ! sudo -s sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='$user'" | grep -q 1; then
     sudo -s sudo -u postgres createuser -s $user
-    sudo -s sudo -u postgres psql -c "ALTER USER $user WITH PASSWORD '$kr|k-*@WPbm5vau'"
+    sudo -s sudo -u postgres psql -c "ALTER USER $user WITH PASSWORD '$DB_PASS'"
 fi
 
 # Install python and virtualenv
@@ -47,7 +49,7 @@ if ! cat /etc/apache2/sites-available/000-default-le-ssl.conf | grep -q "food_si
 fi
 # Note: may need to also configure non-ssl allowed hosts file
 
-# Set Apache permissions for upload
+# Set apache permissions for upload
 sudo chmod a+w /var/www
 
 # Copy jank-ass help pdf to correct place
@@ -66,10 +68,10 @@ fi
 # Restart postgres to reflect changes
 sudo service postgresql restart
 
-# Create Database
+# Create database
 createdb food_db
 
-# Setup Python Virtal Environment
+# Setup python virtal environment
 if ! [ -d site_env ]; then
 	python3 -m virtualenv site_env
 fi
@@ -82,7 +84,18 @@ pip install --upgrade django-filter
 pip install --upgrade django-bootstrap3
 pip install --upgrade psycopg2-binary
 
-# Setup Django
+# Configure django settings
+python -c 'import random; import os; result = "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]); os.environ["SECRET_KEY"] = result'
+deactivate
+OLD_SETTINGS="    'default': {\n        'ENGINE': 'django.db.backends.sqlite3',\n        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),\n    }"
+NEW_SETTINGS="    'default': {\n        'ENGINE': 'django.db.backends.postgresql_psycopg2',\n        'NAME': 'food_db',\n        'USER': '$user',\n        'PASSWORD': '$DB_PASS',\n        'HOST': '',\n        'PORT': '',\n    }"
+sed -i "s/$OLD_SETTINGS/$NEW_SETTINGS/g" food_site/food_site/settings.py
+OLD_DEBUG="DEBUG = True"
+NEW_DEBUG="DEBUG = False"
+sed -i "s/$OLD_DEBUG/$NEW_DEBUG/g" food_site/food_site/settings.py
+
+# Setup django
+source site_env/bin/activate
 ./food_site/manage.py makemigrations -v 0
 ./food_site/manage.py migrate -v 0
 ./food_site/manage.py collectstatic --noinput -v 0
