b4c20e84f1f3a7d4eb361d88949f06653968c838
Nathaniel Brooke <neb17@duke.edu>
2019-02-25 11:32:01 -0500

    Fixed table sorting by added columns

--- food_site/mfg_map/tables.py
+++ food_site/mfg_map/tables.py
@@ -2,15 +2,15 @@ import django_tables2 as tables
 from sku_manage.models import SKU
 
 class SKUTable(tables.Table):
-	lines_column = tables.TemplateColumn(verbose_name="Manufacturing Lines", template_name='mfg_map/linecolumn.html')
-	add_colunm = tables.TemplateColumn(verbose_name="Add", template_name='mfg_map/addcolumn.html')
+	lines_column = tables.TemplateColumn(order_by='skumfgline', verbose_name="Manufacturing Lines", template_name='mfg_map/linecolumn.html')
+	add_colunm = tables.TemplateColumn(order_by='id', verbose_name="Add", template_name='mfg_map/addcolumn.html')
 	class Meta:
 		model = SKU
 		fields = ['name', 'sku_num']
 
 class SelectedTable(tables.Table):
-	lines_column = tables.TemplateColumn(verbose_name="Manufacturing Lines", template_name='mfg_map/linecolumn.html')
-	remove_colunm = tables.TemplateColumn(verbose_name="Remove", template_name='mfg_map/remcolumn.html')
+	lines_column = tables.TemplateColumn(order_by='skumfgline', verbose_name="Manufacturing Lines", template_name='mfg_map/linecolumn.html')
+	remove_colunm = tables.TemplateColumn(order_by='id', verbose_name="Remove", template_name='mfg_map/remcolumn.html')
 	class Meta:
 		model = SKU
 		fields = ['name', 'sku_num']
