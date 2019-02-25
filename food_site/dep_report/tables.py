19206479d4bd084db7ef3e20b20a9a5bd87347ea
Nathaniel Brooke <neb17@duke.edu>
2019-02-25 12:05:38 -0500

    Better sorting fix

--- food_site/dep_report/tables.py
+++ food_site/dep_report/tables.py
@@ -2,19 +2,19 @@ import django_tables2 as tables
 from sku_manage.models import Ingredient, ManufacturingLine
 
 class IngredientTable(tables.Table):
-	add_colunm = tables.TemplateColumn(order_by='id', verbose_name="Add to Report", template_name='dep_report/addcolumn.html')
+	add_colunm = tables.TemplateColumn(orderable=False, verbose_name="Add to Report", template_name='dep_report/addcolumn.html')
 	class Meta:
 		model = Ingredient
 		fields = ['name', 'number', 'vendor_info', 'package_size', 'cost']
 
 class SelectedTable(tables.Table):
-	remove_colunm = tables.TemplateColumn(order_by='id', verbose_name="Remove", template_name='dep_report/remcolumn.html')
+	remove_colunm = tables.TemplateColumn(orderable=False, verbose_name="Remove", template_name='dep_report/remcolumn.html')
 	class Meta:
 		model = Ingredient
 		fields = ['name', 'number', 'vendor_info', 'package_size', 'cost']
 
 class MfgLineTable(tables.Table):
-	view_column = tables.TemplateColumn(order_by='id', verbose_name="View Schedule", template_name='dep_report/viewcolumn.html')
+	view_column = tables.TemplateColumn(orderable=False, verbose_name="View Schedule", template_name='dep_report/viewcolumn.html')
 	class Meta:
 		model = ManufacturingLine
 		fields = ['name', 'shortname']
--- food_site/mfg_map/tables.py
+++ food_site/mfg_map/tables.py
@@ -3,14 +3,14 @@ from sku_manage.models import SKU
 
 class SKUTable(tables.Table):
 	lines_column = tables.TemplateColumn(order_by='skumfgline', verbose_name="Manufacturing Lines", template_name='mfg_map/linecolumn.html')
-	add_colunm = tables.TemplateColumn(order_by='id', verbose_name="Add", template_name='mfg_map/addcolumn.html')
+	add_colunm = tables.TemplateColumn(orderable=False, verbose_name="Add", template_name='mfg_map/addcolumn.html')
 	class Meta:
 		model = SKU
 		fields = ['name', 'sku_num']
 
 class SelectedTable(tables.Table):
 	lines_column = tables.TemplateColumn(order_by='skumfgline', verbose_name="Manufacturing Lines", template_name='mfg_map/linecolumn.html')
-	remove_colunm = tables.TemplateColumn(order_by='id', verbose_name="Remove", template_name='mfg_map/remcolumn.html')
+	remove_colunm = tables.TemplateColumn(orderable=False, verbose_name="Remove", template_name='mfg_map/remcolumn.html')
 	class Meta:
 		model = SKU
 		fields = ['name', 'sku_num']
