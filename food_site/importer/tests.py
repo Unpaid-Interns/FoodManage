ec326605b68b051d6a42d8919f5bb714064dff63
Marcus <mlo11@duke.edu>
2019-02-26 13:50:16 -0500

    working on tests

--- food_site/importer/tests.py
+++ food_site/importer/tests.py
@@ -16,47 +16,78 @@ class HTMLTests(TestCase):
         # self.assertIs()
 
 
+class LowercaseHeaderNoErrorsUpload(TestCase):
+    pass
+
+
 class NoErrorsUpload(TestCase):
-    ingredients_file = 'ingredients_single_correct_upload.csv'
-    product_lines_file = 'product_lines_single_correct_upload.csv'
-    formulas_file = 'formulas_single_correct_upload.csv'
-    skus_file = 'skus_single_correct_upload.csv'
+    ingredients_file = 'ingredients_correct.csv'
+    product_lines_file = 'product_lines_correct.csv'
+    formulas_file = 'formulas_correct.csv'
+    skus_file = 'skus_correct.csv'
+    # models.ManufacturingLine(name='Line1', shortname='line1', comment='').save()
 
-    def test_single_correct_ingredients_upload(self):
+    def test_ingredients_correct(self):
         importer = CSVImport.CSVImport()
-        filename = path_prefix + self.ingredients_file
-        importer.add_filename(filename)
+        importer.add_filename(path_prefix + self.ingredients_file)
         success, conflicts_exist, result_message = importer.import_csv()
         self.assertIs(success, True, result_message)
         self.assertIs(conflicts_exist, False, result_message)
-        self.assertContains(result_message, "Import completed successfully", result_message)
+        self.assertIn("Import completed successfully", result_message, result_message)
+        # importer.clear_filenames()
+        # importer.add_filename(path_prefix + self.product_lines_file)
+        # success, conflicts_exist, result_message = importer.import_csv()
+        # self.assertIs(success, True, result_message)
+        # self.assertIs(conflicts_exist, False, result_message)
+        # self.assertIn("Import completed successfully", result_message, result_message)
+        # importer.clear_filenames()
+        # importer.add_filename(path_prefix + self.formulas_file)
+        # success, conflicts_exist, result_message = importer.import_csv()
+        # self.assertIs(success, True, result_message)
+        # self.assertIs(conflicts_exist, False, result_message)
+        # self.assertIn("Import completed successfully", result_message, result_message)
+        # importer.clear_filenames()
+        # importer.add_filename(path_prefix + self.skus_file)
+        # success, conflicts_exist, result_message = importer.import_csv()
+        # self.assertIs(success, True, result_message)
+        # self.assertIs(conflicts_exist, False, result_message)
+        # self.assertIn("Import completed successfully", result_message, result_message)
 
-    def test_single_correct_product_lines_upload(self):
-        importer = CSVImport.CSVImport()
-        filename = path_prefix + self.product_lines_file
-        importer.add_filename(filename)
-        success, conflicts_exist, result_message = importer.import_csv()
-        self.assertIs(success, True, result_message)
-        self.assertIs(conflicts_exist, False, result_message)
-        self.assertContains(result_message, "Import completed successfully", result_message)
+    # def test_correct_product_lines_upload(self):
+    #     importer = CSVImport.CSVImport()
+    #     filename = path_prefix + self.product_lines_file
+    #     importer.add_filename(filename)
+    #     success, conflicts_exist, result_message = importer.import_csv()
+    #     self.assertIs(success, True, result_message)
+    #     self.assertIs(conflicts_exist, False, result_message)
+    #     self.assertIn("Import completed successfully", result_message, result_message)
+    #
+    # def test_correct_formulas_upload(self):
+    #     importer = CSVImport.CSVImport()
+    #     filename = path_prefix + self.formulas_file
+    #     importer.add_filename(filename)
+    #     success, conflicts_exist, result_message = importer.import_csv()
+    #     self.assertIs(success, True, result_message)
+    #     self.assertIs(conflicts_exist, False, result_message)
+    #     self.assertIn("Import completed successfully", result_message, result_message)
+    #
+    # def test_correct_sku_upload(self):
+    #     importer = CSVImport.CSVImport()
+    #     filename = path_prefix + self.skus_file
+    #     importer.add_filename(filename)
+    #     success, conflicts_exist, result_message = importer.import_csv()
+    #     self.assertIs(success, True, result_message)
+    #     self.assertIs(conflicts_exist, False, result_message)
+    #     self.assertIn("Import completed successfully", result_message, result_message)
 
-    def test_single_correct_formulas_upload(self):
-        importer = CSVImport.CSVImport()
-        filename = path_prefix + self.formulas_file
-        importer.add_filename(filename)
-        success, conflicts_exist, result_message = importer.import_csv()
-        self.assertIs(success, True, result_message)
-        self.assertIs(conflicts_exist, False, result_message)
-        self.assertContains(result_message, "Import completed successfully", result_message)
 
-    def test_single_correct_sku_upload(self):
-        importer = CSVImport.CSVImport()
-        filename = path_prefix + self.skus_file
-        importer.add_filename(filename)
-        success, conflicts_exist, result_message = importer.import_csv()
-        self.assertIs(success, True, result_message)
-        self.assertIs(conflicts_exist, False, result_message)
-        self.assertContains(result_message, "Import completed successfully", result_message)
-
+def test_correct(test, filename):
+    importer = CSVImport.CSVImport()
+    filename = path_prefix + filename
+    importer.add_filename(filename)
+    success, conflicts_exist, result_message = importer.import_csv()
+    test.assertIs(success, True, result_message)
+    test.assertIs(conflicts_exist, False, result_message)
+    test.assertIn("Import completed successfully", result_message, result_message)
 
 
