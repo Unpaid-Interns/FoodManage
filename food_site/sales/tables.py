f9130f02096e26ad5a1c8b18997db526d2acc8f0
Nathaniel Brooke <neb17@duke.edu>
2019-03-09 21:51:26 -0500

    Created product line selection for sales

--- food_site/sales/tables.py
+++ food_site/sales/tables.py
@@ -1,6 +1,6 @@
 import django_tables2 as tables
 from .models import SalesRecord
-from sku_manage.models import Ingredient, ManufacturingLine
+from sku_manage.models import Ingredient, ManufacturingLine, ProductLine
 
 class SkuSalesTable(tables.Table):
 	year = tables.TemplateColumn('{% load ex_tags %}{{ record.date|year }}', order_by='date')
@@ -11,3 +11,15 @@ class SkuSalesTable(tables.Table):
 	class Meta:
 		model = SalesRecord
 		fields = ['year', 'week', 'customer_number', 'customer_name', 'cases_sold', 'price_per_case', 'revenue']
+
+class ProductLineTable(tables.Table):
+	add = tables.TemplateColumn(orderable=False, verbose_name="Add to Report", template_name='sales/addcolumn.html')
+	class Meta:
+		model = ProductLine
+		fields = ['add', 'name']
+
+class SelectedPLTable(tables.Table):
+	remove = tables.TemplateColumn(orderable=False, verbose_name="Remove", template_name='sales/remcolumn.html')
+	class Meta:
+		model = ProductLine
+		fields = ['name', 'remove']
--- food_site/sales/views.py
+++ food_site/sales/views.py
@@ -1,11 +1,88 @@
 from datetime import date, timedelta
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.db.models import Sum
-from sku_manage.models import SKU
+from django.shortcuts import render, redirect
+from django.db.models import Sum, Q
+from django_tables2 import RequestConfig, paginators
+from sku_manage.models import SKU, ProductLine
 from .models import SalesRecord, Customer
-from .tables import SkuSalesTable
+from .tables import SkuSalesTable, ProductLineTable, SelectedPLTable
+
+@login_required
+def pl_select(request):
+	if 'productlines' not in request.session or request.session.get('productlines') == None:
+		request.session['productlines'] = list()
+	queryset = ProductLine.objects.all().exclude(id__in=request.session.get('productlines'))
+	selected = ProductLine.objects.filter(id__in=request.session.get('productlines'))
+	context = {
+		'paginated': True,
+		'keyword': '',
+		'all_skus': SKU.objects.all(),
+		'selected_sku': None,
+	}	
+	paginate = {
+		'paginator_class': paginators.LazyPaginator,
+		'per_page': 25
+	}
+
+	if 'keyword' in request.GET:
+		keyword = request.GET['keyword']
+		queryset = queryset.filter(name__icontains=keyword)
+		context['keyword'] = keyword
+
+	if 'skufilter' in request.GET:
+		sku_num = request.GET['skufilter']
+		if sku_num != 'all':
+			queryset = queryset.filter(sku__sku_num=sku_num)
+			context['selected_sku'] = int(sku_num)
+
+	if 'remove_pagination' in request.GET:
+		paginate = False
+		context['paginated'] = False
+
+	if request.method == 'POST':
+		if 'add_all' in request.POST:
+			productlines = list()
+			productlines.extend(request.session.get('productlines'))
+			for pl in queryset:
+				productlines.append(pl.pk)
+			request.session['productlines'] = productlines
+			return redirect('sales_report_select')
+		if 'remove_all' in request.POST:
+			request.session['productlines'] = list()
+			return redirect('sales_report_select')
+		if 'gen_report' in request.POST:
+			return redirect('sales_report')
+
+	input_table = ProductLineTable(queryset)
+	selected_table = SelectedPLTable(selected)
+	context['input_table'] = input_table
+	context['selected_table'] = selected_table
+	context['queryset'] = queryset
+	RequestConfig(request, paginate=paginate).configure(input_table)
+	return render(request, 'sales/data.html', context)
+
+@login_required
+def product_line_add(request, pk):
+	productlines = [pk]
+	productlines.extend(request.session.get('productlines'))
+	request.session['productlines'] = productlines
+	return redirect('sales_report_select')
+
+@login_required
+def product_line_remove(request, pk):
+	productlines = list()
+	productlines.extend(request.session.get('productlines'))
+	productlines.remove(pk)
+	request.session['productlines'] = productlines
+	return redirect('sales_report_select')
+
+@login_required
+def sales_report(request):
+	context = {
+		'sku_list': SKU.objects.filter(product_line__pk__in=request.session.get('productlines'))
+	}
+	return render(request, 'sales/report.html', context)
 
 @login_required
 def sku_drilldown(request, pk):
