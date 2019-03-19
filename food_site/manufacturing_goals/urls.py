cd060181c0bc07edf39a35d2411f46d429c2c9f0
Nathaniel Brooke <neb17@duke.edu>
2019-03-19 17:44:09 -0400

    Cleaning URLs

--- food_site/manufacturing_goals/urls.py
+++ food_site/manufacturing_goals/urls.py
@@ -3,12 +3,12 @@ from manufacturing_goals import views
 from django.urls import path
 
 urlpatterns = [
-	path('manufacturing/', views.manufacturing, name='manufacturing'),
-	path('manufacturing/timeline/', views.timeline, name='timeline'),
-	path('manufacturing/manufqty/', views.manufqty, name='manufqty'),
-	path('manufacturing/manufdetails/', views.manufdetails, name='manufdetails'),
-	path('manufacturing/add/<int:pk>', views.goal_add, name='mfg_goal_add'),
-	path('manufacturing/remove/<int:pk>', views.goal_remove, name='mfg_goal_remove'),
-	path('manufacturing/enable/', views.enable_menu, name='enable_menu'),
-	path('manufacturing/enable/<int:pk>', views.enable_goal, name='enable_goal'),
+	path('', views.manufacturing, name='manufacturing'),
+	path('timeline/', views.timeline, name='timeline'),
+	path('manufqty/', views.manufqty, name='manufqty'),
+	path('manufdetails/', views.manufdetails, name='manufdetails'),
+	path('add/<int:pk>', views.goal_add, name='mfg_goal_add'),
+	path('remove/<int:pk>', views.goal_remove, name='mfg_goal_remove'),
+	path('enable/', views.enable_menu, name='enable_menu'),
+	path('enable/<int:pk>', views.enable_goal, name='enable_goal'),
 ]
