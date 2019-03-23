7a9b6a180fe136f23cca54c92bda8af60a2ae1e4
Nathaniel Brooke <neb17@duke.edu>
2019-03-23 11:40:41 -0400

    Fixed Backup scripts

--- backup/backup.py
+++
@@ -1,7 +0,0 @@
-from os import system
-from datetime import date
-
-system('pwd')
-system('../food_site/manage.py dumpdata > backup')
-system('scp backup vcm@vcm-8261.vm.duke.edu:backup-{date}'.format(date=date.today()))
-system('rm backup')
