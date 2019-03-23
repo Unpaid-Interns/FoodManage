c211af299c83de9e94aaa3a8d9748e6d465f5d2d
Nathaniel Brooke <neb17@duke.edu>
2019-03-23 11:10:58 -0400

    Fixed filepath

--- backup/backup.py
+++ backup/backup.py
@@ -1,6 +1,7 @@
 from os import system
 from datetime import date
 
-system('../food-site/manage.py dumpdata > backup')
+system('pwd')
+system('../food_site/manage.py dumpdata > backup')
 system('scp backup vcm@vcm-8261.vm.duke.edu:backup-{date}'.format(date=date.today()))
 system('rm backup')
