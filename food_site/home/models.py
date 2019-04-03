32f98dd7a6eddcfcf0019417e1ff2b205837bcbd
Nathaniel Brooke <neb17@duke.edu>
2019-04-03 14:26:44 -0400

    Backend database updates

--- food_site/home/models.py
+++ food_site/home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from sku_manage.models import ManufacturingLine
 
-# Create your models here.
+class PlantManager(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	mfgline = models.ForeignKey(ManufacturingLine, on_delete=models.CASCADE)
