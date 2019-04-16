ec4d6b19859aaf13c3f144e010efcbbb81f7ed24
Nathaniel Brooke <neb17@duke.edu>
2019-04-16 00:02:54 -0400

    Removed auto updates

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,16 +12,8 @@ fi
 # Update from repository
 git pull
 
-# Update python packages
-source site_env/bin/activate
-pip install --upgrade django
-pip install --upgrade django-tables2
-pip install --upgrade django-bootstrap3
-pip install --upgrade psycopg2-binary
-pip install --upgrade requests
-pip install --upgrade django-background-tasks
-
 # Setup Django
+source site_env/bin/activate
 ./food_site/manage.py migrate
 ./food_site/manage.py collectstatic --noinput
 deactivate
